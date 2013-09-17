ad134c29a13548baf26ba735ea925100e60aafd7
Simon Wells <siwells@gmail.com>
2013-09-17 13:50:13 +0100

    Use the webUI & API blueprints

--- server/src/uarg/uarg.py
+++ server/src/uarg/uarg.py
@@ -7,18 +7,16 @@ from flask import Flask, json, jsonify, render_template, request, session, url_f
 
 import configuration
 
+from api.views import api
+from webui.views import web
+
 app = Flask(__name__)
+app.register_blueprint(api)
+app.register_blueprint(web)
+
 
 configuration.init(app)
 configuration.logs(app)
-
-
-
-
-@app.route('/')
-def root():
-    msg = { 'hello':'world' }
-    return jsonify( msg )
 
 
 if __name__ == '__main__':
