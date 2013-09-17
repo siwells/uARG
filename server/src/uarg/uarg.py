9b1d4db2d84c7748a23a1de6c5044e26afcb2cee
Simon Wells <siwells@gmail.com>
2013-09-17 13:16:53 +0100

    Use config data in flask app

--- server/src/uarg/uarg.py
+++ server/src/uarg/uarg.py
@@ -1,10 +1,18 @@
 # coding: utf-8
 # as per http://www.python.org/dev/peps/pep-0263/
 
+import ConfigParser
+
 from flask import Flask, json, jsonify, render_template, request, session, url_for
 
+import configuration
 
 app = Flask(__name__)
+
+configuration.init(app)
+configuration.logs(app)
+
+
 
 
 @app.route('/')
@@ -14,5 +22,8 @@ def root():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(
+        host=app.config['ip_address'], 
+        port=int(app.config['port']), 
+        threaded=app.config['threaded'])
     
