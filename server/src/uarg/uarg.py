e1c41ad81b4724a7bc7e38ba1a47a6a3d0b58126
Simon Wells <siwells@gmail.com>
2013-09-17 14:22:48 +0100

    Content negotiate error routes

--- server/src/uarg/uarg.py
+++ server/src/uarg/uarg.py
@@ -3,7 +3,7 @@
 
 import ConfigParser
 
-from flask import Flask, json, jsonify, render_template, request, session, url_for
+from flask import Flask, json, jsonify, Markup, render_template, request, session, url_for
 
 import configuration
 
@@ -14,10 +14,35 @@ app = Flask(__name__)
 app.register_blueprint(api)
 app.register_blueprint(web)
 
-
 configuration.init(app)
 configuration.logs(app)
 
+@app.errorhandler(401)
+def error_401(e):
+	#return render_template('401.html'), 401
+    return Markup("401 Error"), 401
+
+@app.errorhandler(403)
+def error_403(e):
+	#return render_template('403.html'), 403
+    return Markup("403 Error"), 403
+
+@app.errorhandler(404)
+def error_404(e):
+    if 'text/html' in request.headers.get("Accept", ""):
+        return Markup("404 Error"), 404
+    else:
+        return jsonify( {'status':'ko', 'statusCode': 404, 'message':'something went wrong'} )
+
+@app.errorhandler(410)
+def error_410(e):
+	#return render_template('410.html'), 410
+    return Markup("410 Error"), 410
+
+@app.errorhandler(500)
+def error_500(e):
+	#return render_template('500.html'), 500
+    return Markup("500 Error"), 500
 
 if __name__ == '__main__':
     app.run(
