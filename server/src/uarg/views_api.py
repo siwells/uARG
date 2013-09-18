d6523624c5f6da50b1875eea2ee00b1feef6debb
Simon Wells <siwells@gmail.com>
2013-09-18 11:56:20 +0100

    Added a root route to the api route

--- server/src/uarg/views_api.py
+++ server/src/uarg/views_api.py
@@ -5,6 +5,17 @@ from flask import Blueprint, json, jsonify, current_app
 
 api = Blueprint("api", __name__, url_prefix='/api')
 
+@api.route('/')
+def root():
+    """
+    """
+    msg = "ROOT - This should return basic information about using the API from this route onwards"
+    response = {'status':'ok', 'statusCode': 200, 'message':msg}
+
+    current_app.logger.warn("ROOT ROUTE")
+
+    return jsonify( response )
+
 @api.route('/log')
 def log():
     """
