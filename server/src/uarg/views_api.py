30fee42ea361924326e95ddf120ffb23f4a44c96
Simon Wells <siwells@gmail.com>
2013-09-20 17:21:38 +0100

    Adjust msg from GET /api/ for consistency

--- server/src/uarg/views_api.py
+++ server/src/uarg/views_api.py
@@ -1,36 +1,41 @@
 # coding: utf-8
 # as per http://www.python.org/dev/peps/pep-0263/
 
-from flask import Blueprint, json, jsonify, current_app
-
+from flask import Blueprint, current_app, json, jsonify, request
 api = Blueprint("api", __name__, url_prefix='/api')
 
 @api.route('/')
 def root():
     """
     """
-    msg = "ROOT - This should return basic information about using the API from this route onwards"
+    msg = "GET /api/ - This should return basic information about using the API from this route onwards"
     response = {'status':'ok', 'statusCode': 200, 'message':msg}
 
-    current_app.logger.warn("ROOT ROUTE")
+    current_app.logger.warn("GET /api/")
 
     return jsonify( response )
 
-@api.route('/claim')
+@api.route('/claim', methods=['POST'])
 def claim():
     """
     """
 
-    msg = "CLAIM - This should return basic information about using the API from this route onwards"
+    msg = None
+
+    if request.method == 'POST':
+        msg = "CLAIM - This should return basic information about using the API from this route onwards"
+        current_app.logger.info("POST /api/claim")
+
     response = {'status':'ok', 'statusCode': 200, 'message':msg}
 
-    current_app.logger.warn("CLAIM ROUTE")
+    
 
     return jsonify( response )
 
 @api.route('/claims')
 def claims():
     """
+    Requires support for retrieving with offset + rows
     """
 
     msg = "CLAIMS - Retrieve claims from server"
