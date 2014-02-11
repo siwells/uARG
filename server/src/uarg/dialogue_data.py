0ca7a326dd8650dac836f157718ce668ab3d575d
Simon Wells <siwells@gmail.com>
2014-02-11 18:16:14 +0000

    Give each dialogue a UUID

--- server/src/uarg/dialogue_data.py
+++ server/src/uarg/dialogue_data.py
@@ -9,12 +9,13 @@ from datetime import datetime
 
 def new_dialogue(db, speaker, speaker_uuid, root_txt, root_type):
 
-    now = str(datetime.now().isoformat())
+    dialogue_uuid = str(UUID.uuid4())
     utterance_uuid = str(UUID.uuid4())
+    now = str(datetime.now().isoformat())
 
     utterance = {'index':0, 'timestamp':now, 'utterance_uuid': utterance_uuid, 'speaker':speaker, 'speaker_uuid':speaker_uuid, 'content':root_txt, 'root_type':root_type}
     
-    doc = {"created": now, "transcript":[utterance]}
+    doc = {"dialogue_uuid": dialogue_uuid, "created": now, "transcript":[utterance]}
     doc_id = db.save(doc)
 
 def add_utterance(db):
