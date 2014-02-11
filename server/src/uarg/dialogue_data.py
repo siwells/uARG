9bd9f545972949bb56c1386347b2c5b9d9b5a4e4
Simon Wells <siwells@gmail.com>
2014-02-11 18:13:47 +0000

    Set index of first utterance in dialogue to 0

--- server/src/uarg/dialogue_data.py
+++ server/src/uarg/dialogue_data.py
@@ -12,7 +12,7 @@ def new_dialogue(db, speaker, speaker_uuid, root_txt, root_type):
     now = str(datetime.now().isoformat())
     utterance_uuid = str(UUID.uuid4())
 
-    utterance = {'timestamp':now, 'utterance_uuid': utterance_uuid, 'speaker':speaker, 'speaker_uuid':speaker_uuid, 'content':root_txt, 'root_type':root_type}
+    utterance = {'index':0, 'timestamp':now, 'utterance_uuid': utterance_uuid, 'speaker':speaker, 'speaker_uuid':speaker_uuid, 'content':root_txt, 'root_type':root_type}
     
     doc = {"created": now, "transcript":[utterance]}
     doc_id = db.save(doc)
