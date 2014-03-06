47712e1df2dece0a9105232e4586b33b6784b297
Simon Wells <siwells@gmail.com>
2014-03-06 16:58:30 +0000

    Added route for /dialogues route

--- server/test/uarg/uarg_test.py
+++ server/test/uarg/uarg_test.py
@@ -2,14 +2,26 @@ import os
 import uarg 
 import unittest
 
-class UargRoot(unittest.TestCase):
+class API_Root(unittest.TestCase):
 
     def test_root(self):
         self.app = uarg.app.test_client()
-        out = self.app.get('/')        
+        out = self.app.get('/')
         assert '200 OK' in out.status
         assert 'charset=utf-8' in out.content_type
         assert 'text/html' in out.content_type
+
+
+class API_Dialogues(unittest.TestCase):
+
+    def test_dialogues(self):
+        self.app = uarg.app.test_client()
+        out = self.app.get('/api/dialogues')
+        assert '200 OK' in out.status
+        assert 'application/json' in out.content_type
+
+
+
 
 if __name__ == "__main__":
     
