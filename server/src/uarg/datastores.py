4465a1b17baa476f032ce90b066a54d54ea5de98
Simon Wells <siwells@gmail.com>
2014-03-07 08:18:57 +0000

    get design doc from within contains view function

--- server/src/uarg/datastores.py
+++ server/src/uarg/datastores.py
@@ -71,17 +71,20 @@ def get_design(db, design):
         return None
 
 
-def contains_view(design_doc, view_name):
+def contains_view(db, design, view_name):
     """
-    Check whether the supplied design_doc contains the nominated view
+    Check whether the design_doc in the specified DB contains the nominated view
 
     True if the design contains the view, False otherwise
     """
 
-    key = None
+    design_doc = get_design(db, design)
+
     if design_doc is not None:
+        key = None
         try:
             key = design_doc['views'][view_name]
+            print design_doc
             if key is not None:
                 return True
         except KeyError:
